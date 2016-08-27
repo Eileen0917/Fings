ae709e7077d86a6e23b24e5b390fa0b9d48851e9
EileenKuo <ekuo0917@gamil.com>
2016-08-27 15:34:38 +0800

    finish 'validates places and things'

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,4 @@
 class Place < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
 end
--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,2 +1,5 @@
 class Thing < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :quantity, numericality: { greater_than: 0 }
 end
